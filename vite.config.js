52fedebc3a0cd77e06a66397afbc57d3d5fbf1cb
ToghrulZulfaliev <toghrulzulfaliev8@gmail.com>
2026-01-16 01:49:49 +0400

    Fix Vite base path for production

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,4 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// GitHub Pages üçün: https://username.github.io/<REPO_NAME>/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  base: "/aboutme/",
-});
+})
